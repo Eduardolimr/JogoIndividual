ba1e1e7ec695a3a0ea1a4e21dc1006b60674167e
Eduardo Lima <eduardolimrib@gmail.com>
2018-05-09 18:21:08 -0300

    Animations and timers

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include "gameobject.hpp"
 #include "components/image.hpp"
 #include "components/text.hpp"
+#include "components/animation.hpp"
+#include "components/animation_controller.hpp"
 #include "components/audio.hpp"
 
 #include "gameglobals.hpp"
@@ -20,13 +22,10 @@ int main(int, char**)
     Scene menu("Main Menu");
 
     auto player = GameObject("Player");
-    player.set_position(0, 0);
+    player.set_position(100, 100);
 
     auto playerImage = ImageComponent("image.png");
     player.add_component(playerImage);
-
-    MoveSideToSide movesidetoside;
-    player.add_component(movesidetoside);
 
     auto title = GameObject("Title");
     title.set_position(250, 250);
@@ -42,8 +41,31 @@ int main(int, char**)
     Move move;
     title.add_component(move);
 
+
+    GameObject boy("Boy");
+    boy.set_position(20, 20);
+
+    AnimationControllerComponent animCtrl;
+
+    AnimationComponent boyAnimation("boy.png", 3, 2, 0.5, true);
+    boyAnimation.set_end_frame(2);
+    animCtrl.add_animation("running right", boyAnimation);
+
+    AnimationComponent boyAnimation2("boy.png", 3, 2, 1.0, true);
+    boyAnimation2.set_frame_range(3, 5);
+    animCtrl.add_animation("running left", boyAnimation2);
+
+    MoveSideToSide movesidetoside;
+
+    boy.add_component(movesidetoside);
+    boy.add_component(boyAnimation);
+    boy.add_component(boyAnimation2);
+    boy.add_component(animCtrl);
+
+
     menu.add_game_object(player);
     menu.add_game_object(title);
+    menu.add_game_object(boy);
 
     Game::instance.add_scene(menu);
 
